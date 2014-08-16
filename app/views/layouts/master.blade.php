40a1ed50873658bf0c50384a80cb882582517f44
Epick362 <Epick@Filip-MacBook-Pro.local>
2014-08-16 13:40:53 +0200

    Gfycat Fixes

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -68,5 +68,14 @@
 				</div>
 			</div>
 		</div>
+
+		<script>
+		 (function(d, t) {
+		    var g = d.createElement(t),
+		        s = d.getElementsByTagName(t)[0];
+		    g.src = 'http://assets.gfycat.com/js/gfyajax-0.517d.js';
+		    s.parentNode.insertBefore(g, s);
+		 }(document, 'script'));
+		</script>
 	</body>
 </html>
