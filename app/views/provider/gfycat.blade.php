ff97e0eebf31966c1dcc51ff34e2bd6d93279fa3
Epick362 <Epick@Filip-MacBook-Pro.local>
2014-08-16 13:34:55 +0200

    Gfycat provider

---
+++ app/views/provider/gfycat.blade.php
@@ -0,0 +1,4 @@
+
+<div class="embed-responsive embed-responsive-16by9">
+	<iframe class="embed-responsive-item" src="http://gfycat.com/ifr/{{ $data['gfycat-id'] }}" frameborder="0" scrolling="no" width="600" height="338" style="-webkit-backface-visibility: hidden;-webkit-transform: scale(1);" ></iframe>
+</div>
