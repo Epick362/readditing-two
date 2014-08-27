87269a5cc45d087a8f9ea1324eda66c1c774d4ed
Epick362 <Epick@Filip-MacBook-Pro.local>
2014-08-27 11:48:12 +0200

    Revert fix

--- workbench/readditing/formatter/src/Readditing/Formatter/Formatter.php
+++ workbench/readditing/formatter/src/Readditing/Formatter/Formatter.php
@@ -28,19 +28,15 @@ class Formatter {
 	public static function getSource($url) {
 		$parts = parse_url($url);
 
-		if(isset($parts['host']))  {
-			$host = str_replace('www.', '', $parts['host']);
-			$matches = array();
-			preg_match('/(.*?)((\.co)?.[a-z]{2,4})$/i', $host, $matches);
-			if(strchr($matches[1], '.')) {
-				$_url = substr(strrchr($matches[1], '.'), 1);
-			}else{
-				$_url = $matches[1];
-			}
-
-			return $_url;
+		$host = str_replace('www.', '', $parts['host']);
+		$matches = array();
+		preg_match('/(.*?)((\.co)?.[a-z]{2,4})$/i', $host, $matches);
+		if(strchr($matches[1], '.')) {
+			$_url = substr(strrchr($matches[1], '.'), 1);
+		}else{
+			$_url = $matches[1];
 		}
 
-		return 'OtherProvider';
+		return $_url;
 	}
 }
