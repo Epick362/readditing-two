adc2753fc77d76008f80c4ee7822e7981775eac7
Epick362 <Epick@Filip-MacBook-Pro.local>
2014-08-27 13:25:33 +0200

    Improved Yt.com embed

--- app/views/provider/youtube.blade.php
+++ app/views/provider/youtube.blade.php
@@ -1,4 +1,4 @@
 
 <div class="embed-responsive embed-responsive-16by9">
-		<iframe class="embed-responsive-item" width="560" height="315" src="//www.youtube.com/embed/{{ $data['youtube-id'] }}?rel=0" frameborder="0" allowfullscreen></iframe>
+	<iframe class="embed-responsive-item" width="560" height="315" src="//www.youtube.com/embed/{{ $data['youtube-id'] }}?rel=0&amp;modestbranding=1&amp;theme=light" frameborder="0" allowfullscreen></iframe>
 </div>
