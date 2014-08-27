b696245500d3158da82c0caa7e90070aade965db
Epick362 <Epick@Filip-MacBook-Pro.local>
2014-08-27 13:35:49 +0200

    Twitter Provider

---
+++ workbench/readditing/formatter/src/Readditing/Formatter/Provider/twitter.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Readditing\Formatter\Provider;
+
+use Readditing\Formatter\Provider;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Message\Request;
+use GuzzleHttp\Message\Response;
+use GuzzleHttp\Exception\ClientException;
+
+class Twitter extends Provider {
+	/**
+	* @var  string  provider name
+	*/
+	public $name = 'twitter';
+
+	public function __construct($data) {
+		$this->data = $data;
+	}
+	/**
+	 * This is where the magic happens
+	 *
+	 * @return  Array
+	 */
+	public function getPost()
+	{
+		try {
+			$client = new Client();
+			$response = $client->get("https://api.twitter.com/1/statuses/oembed.json?maxwidth=600&align=center&url=".urlencode($this->data['data']['url']))->json();
+		}catch (ClientException $e) {
+			return $this->fail();
+		}
+
+		return array(
+			'title' => $this->data['data']['title'], 
+			'content' => $response['html'], 
+			'source' => 'twitter.com'
+		);
+	}
+
+	private function fail() {
+		return array(
+			'title' => $this->data['data']['title'], 
+			'content' => 'Sorry we couldn\'t get this image for you', 
+			'source' => 'imgur.com'
+		);
+	}
+}
