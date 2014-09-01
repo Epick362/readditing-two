2ff6f1ddce4f2fb680109cf0e6f0de72dabff54f
Epick362 <Epick@Filip-MacBook-Pro.local>
2014-09-01 17:41:55 +0200

    Environment Configuration

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -7,7 +7,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'readditing',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
