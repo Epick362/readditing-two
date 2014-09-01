d6d04090aeef929c1b2eae999ce2542831f68ca1
Epick362 <Epick@Filip-MacBook-Pro.local>
2014-09-01 20:20:29 +0200

    Cache Experiment

--- workbench/readditing/reddit/src/Readditing/Reddit/Reddit.php
+++ workbench/readditing/reddit/src/Readditing/Reddit/Reddit.php
@@ -20,18 +20,20 @@ class Reddit {
 
 		$url .= '?'.$postdata;
 
-		$opts = array(
-			'http' => array(
-				'method'  => $method,
-				'header'  => array('Content-type: application/x-www-form-urlencoded',
-									'Authorization: bearer ' . self::$access_token,
-									'User-Agent: Readditing.com by Epick_362')
-			)
-		);
-		$_default_opts = stream_context_get_params(stream_context_get_default());
-		$context = stream_context_create(array_merge_recursive($_default_opts['options'], $opts));
+		$response = \Cache::remember(urlencode($url), 5, function() use($method) {
+			$opts = array(
+				'http' => array(
+					'method'  => $method,
+					'header'  => array('Content-type: application/x-www-form-urlencoded',
+										'Authorization: bearer ' . self::$access_token,
+										'User-Agent: Readditing.com by Epick_362')
+				)
+			);
+			$_default_opts = stream_context_get_params(stream_context_get_default());
+			$context = stream_context_create(array_merge_recursive($_default_opts['options'], $opts));
 
-		$response = json_decode(@file_get_contents($url, false, $context), true);
+			return json_decode(@file_get_contents($url, false, $context), true);
+		});
 
 		return $response;
 	}
