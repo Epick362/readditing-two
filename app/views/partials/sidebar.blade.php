8f574e51ab8c1c6e3491d73de0c0c2e8d2c9abfb
Epick362 <Epick@Filip-MacBook-Pro.local>
2014-09-02 23:46:58 +0200

    Last Fix of Sidebar

--- app/views/partials/sidebar.blade.php
+++ app/views/partials/sidebar.blade.php
@@ -20,7 +20,7 @@
 			</div>
 			<div class="panel-body">
 				@foreach($popular['data']['children'] as $_sub)
-					<a href="{{ URL::to('r/'.$_sub['display_name']) }}">{{ $_sub['data']['display_name'] }}</a><br />
+					<a href="{{ URL::to('r/'.$_sub['data']['display_name']) }}">{{ $_sub['data']['display_name'] }}</a><br />
 				@endforeach
 			</div>
 		</div>
