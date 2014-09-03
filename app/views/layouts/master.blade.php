3b9b97dc1764bbd3256c5e9043448279f93d2976
Epick362 <Epick@Filip-MacBook-Pro.local>
2014-09-03 14:17:51 +0200

    HTML changes

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -26,7 +26,6 @@
 		<link rel="stylesheet" type="text/css" href="{{ URL::asset('app/styles/main.css') }}">
 
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
-		<script src="http://cdn.bootcss.com/FitText.js/1.1/jquery.fittext.min.js"></script>
 
 		<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.8/angular.min.js"></script> <!-- load angular -->
 		<script src="//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.10.0/ui-bootstrap.min.js"></script>
@@ -81,6 +80,16 @@
 
 		<div id="wrap">
 			<div class="container">
+				@if(isset($subreddit) && $subreddit)
+				<div class="row">
+					<h1>
+						{{ $subreddit }} 
+						@if(Session::has('user'))
+							<a href="#" class="btn btn-default btn-block"><i class="fa fa-bookmark"></i> Subscribe</a>
+						@endif
+					</h1>
+				</div>
+				@endif
 				<div class="row">
 					<div class="col-md-8 col-md-offset-1">
 						@yield('content')
@@ -91,13 +100,5 @@
 				</div>
 			</div>
 		</div>
-
-		<script>
-			$(function() {
-				$('#subreddit-name').fitText();
-
-				console.log('im working');
-			});
-		</script>
 	</body>
 </html>
--- app/views/partials/sidebar.blade.php
+++ app/views/partials/sidebar.blade.php
@@ -1,18 +1,5 @@
 
 <div class="col-md-2" style="position:fixed; top:120px">
-	@if(isset($subreddit) && $subreddit)
-		<div class="sidebar panel panel-default">
-			<div class="panel-body">
-				<div id="subreddit-name" class="panel-heading">
-					{{ $subreddit }}
-				</div>
-				@if(Session::has('user'))
-				<a href="#" class="btn btn-default btn-block"><i class="fa fa-bookmark"></i> Subscribe</a>
-				@endif
-			</div>
-		</div>
-	@endif
-
 	@if(isset($popular) && $popular)
 		<div class="sidebar panel panel-default">
 			<div class="panel-heading">
