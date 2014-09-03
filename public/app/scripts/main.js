7664adad9a60e817a3f4799f6931bfc174278ba0
Epick362 <Epick@Filip-MacBook-Pro.local>
2014-09-03 17:25:40 +0200

    .

--- public/app/scripts/main.js
+++ public/app/scripts/main.js
@@ -2,34 +2,9 @@ $(function() {
 	$("figure.upvoteable").upvoteable();
 
 	$("figure.upvote").bind("upvote:added", function(e) {
-		var post = $(this).closest('.panel').data('post');
-		 $.ajax({
-			  type: 'POST',
-			  url: 'http://readditing.com/ajax/vote',
-			  data: {'fullname': 't3_'+post, 'dir': '1'},
-			  success: function(data){
-			  	//alert('upvoted!');
-			  },
-			  error: function() {
-			  	//alert('error');
-			  },
-			  dataType: 'html'
-		 });
 	});
 
 	$("figure.upvote").bind("upvote:removed", function(e) {
-		var post = $(this).closest('.panel').data('post');
-		 $.ajax({
-			  type: 'POST',
-			  url: 'http://readditing.com/ajax/vote',
-			  data: {'fullname': 't3_'+post, 'dir': '0'},
-			  success: function(data){
-			  	//alert('upvote removed!');
-			  },
-			  error: function() {
-			  	//alert('error');
-			  },
-			  dataType: 'html'
-		 });
+
 	});
 });
