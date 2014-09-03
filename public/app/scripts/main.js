dac057e6c45929e4ff92e45cac966f3cca651fd2
Epick362 <Epick@Filip-MacBook-Pro.local>
2014-09-03 17:25:21 +0200

    JS FIX

--- public/app/scripts/main.js
+++ public/app/scripts/main.js
@@ -1,33 +1,35 @@
 $(function() {
-    // initialize kudos
-    $("figure.kudoable").kudoable();
- 
-    // check to see if user has already kudod
-    if($.cookie(postId) == 'true') {
-        // make kudo already kudod
-        $("figure.kudoable").removeClass("animate").addClass("complete");
- 
-        /*
-        * Your server would take care of the proper kudos count, but because this is a
-        * static page, we need to set it here so it doesn't become -1 when you remove
-        * the kudos after a reload
-        */
-        $(".num").html(1);
-    }
- 
-    // after kudo'd
-    $("figure.kudo").bind("kudo:added", function(e) {
-        var element = $(this);
- 
-        // set cookie so user cannot kudo again for 365 days
-        $.cookie(postId, 'true', { expires: 365 });
-    });
- 
-    // after removing a kudo
-    $("figure.kudo").bind("kudo:removed", function(e) {
-        var element = $(this);
- 
-        // remove cookie
-        $.removeCookie(postId);
-    });
+	$("figure.upvoteable").upvoteable();
+
+	$("figure.upvote").bind("upvote:added", function(e) {
+		var post = $(this).closest('.panel').data('post');
+		 $.ajax({
+			  type: 'POST',
+			  url: 'http://readditing.com/ajax/vote',
+			  data: {'fullname': 't3_'+post, 'dir': '1'},
+			  success: function(data){
+			  	//alert('upvoted!');
+			  },
+			  error: function() {
+			  	//alert('error');
+			  },
+			  dataType: 'html'
+		 });
+	});
+
+	$("figure.upvote").bind("upvote:removed", function(e) {
+		var post = $(this).closest('.panel').data('post');
+		 $.ajax({
+			  type: 'POST',
+			  url: 'http://readditing.com/ajax/vote',
+			  data: {'fullname': 't3_'+post, 'dir': '0'},
+			  success: function(data){
+			  	//alert('upvote removed!');
+			  },
+			  error: function() {
+			  	//alert('error');
+			  },
+			  dataType: 'html'
+		 });
+	});
 });
