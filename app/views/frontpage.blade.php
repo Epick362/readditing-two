831efc0d4eaa4280492e95d1249d8836044aff5f
Epick362 <Epick@Filip-MacBook-Pro.local>
2014-09-08 18:51:12 +0200

    .

--- app/views/frontpage.blade.php
+++ app/views/frontpage.blade.php
@@ -4,12 +4,12 @@
 	<div id="subreddit" ng-app="subredditApp" ng-controller="subredditController" subreddit="{{ $subreddit or false }}">
 		@if(Session::has('user'))
 			<h1>User Data</h1>
-			<pre>{{ print_r(Session::get('user')) }}</pre>
+			<pre>{{ print_r(Session::get('user'), true) }}</pre>
 		@endif
 
 		@if(Session::has('access_token'))
 			<h1>Access Token</h1>
-			<pre>{{ print_r(Session::get('access_token')) }}</pre>
+			<pre>{{ print_r(Session::get('access_token'), true) }}</pre>
 		@endif
 
 		<alert ng-repeat="alert in alerts" type="<% alert.type %>" close="closeAlert($index)"><% alert.msg %></alert>
