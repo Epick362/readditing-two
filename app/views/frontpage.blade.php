ec6527f90dd321817f8a56ced31cabbbf32605a8
Epick362 <Epick@Filip-MacBook-Pro.local>
2014-09-08 18:44:23 +0200

    .

--- app/views/frontpage.blade.php
+++ app/views/frontpage.blade.php
@@ -3,7 +3,13 @@
 @section('content')
 	<div id="subreddit" ng-app="subredditApp" ng-controller="subredditController" subreddit="{{ $subreddit or false }}">
 		@if(Session::has('user'))
+			<h1>User Data</h1>
 			<pre>{{ print_r(Session::get('user')) }}</pre>
+		@endif
+
+		@if(Session::has('access_token'))
+			<h1>Access Token</h1>
+			<pre>{{ print_r(Session::get('access_token')) }}</pre>
 		@endif
 
 		<alert ng-repeat="alert in alerts" type="<% alert.type %>" close="closeAlert($index)"><% alert.msg %></alert>
