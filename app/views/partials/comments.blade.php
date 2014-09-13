bd012063c8bda4582aba54d3ea7a3fc68e72f89b
Epick362 <Epick@Filip-MacBook-Pro.local>
2014-09-13 14:11:33 +0200

    ngComments Fixes

--- app/views/partials/comments.blade.php
+++ app/views/partials/comments.blade.php
@@ -6,10 +6,12 @@
 <div class="modal-body">
     <div infinite-scroll='reddit.getComments(post.id)' infinite-scroll-disabled='reddit.busy' infinite-scroll-distance='1'>
 		<div ng-repeat="comment in reddit.comments">
-			<a class="pull-left" href="#"><img class="media-object" src="http://placehold.it/64x64"></a>
-			<div class="media-body">
-				<h4 class="media-heading"><a href="#"><% comment.author %></a> <small><i class="fa fa-arrow-up"></i> <% comment.score %></small></h4>
-				<div ng-bind-html="comment.body"></div>
+			<div class="media">
+				<a class="pull-left" href="#"><img class="media-object" src="http://placehold.it/64x64"></a>
+				<div class="media-body">
+					<h4 class="media-heading"><a href="#"><% comment.author %></a> <small><i class="fa fa-arrow-up"></i> <% comment.score %></small></h4>
+					<div ng-bind-html="comment.body"></div>
+				</div>
 			</div>
 		</div>
 	</div>
