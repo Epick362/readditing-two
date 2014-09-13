87f32ee875ab9db8cdd2d33196f0f8e2a8a17c19
Epick362 <Epick@Filip-MacBook-Pro.local>
2014-09-13 14:43:47 +0200

    Visual Improvements

--- app/views/partials/comments.blade.php
+++ app/views/partials/comments.blade.php
@@ -8,7 +8,7 @@
 		<li class="media" ng-repeat="comment in reddit.comments">
 			<a class="pull-left" href="#"><img class="media-object" src="http://placehold.it/64x64"></a>
 			<div class="media-body">
-				<h4 class="media-heading"><a href="#"><% comment.author %></a> <small><i class="fa fa-arrow-up"></i> <% comment.score %></small></h4>
+				<h4 class="media-heading"><a href="#"><% comment.author %></a> <small class="text-alternate"><i class="fa fa-arrow-up"></i> <% comment.score %></small></h4>
 				<div ng-bind-html="comment.body"></div>
 			</div>
 		</li>
