848037f45e4da37576bcadf607771b7865f3a059
Epick362 <Epick@Filip-MacBook-Pro.local>
2014-09-13 14:10:01 +0200

    ??

--- app/views/partials/comments.blade.php
+++ app/views/partials/comments.blade.php
@@ -5,7 +5,7 @@
 </div>
 <div class="modal-body">
     <div infinite-scroll='reddit.getComments(post.id)' infinite-scroll-disabled='reddit.busy' infinite-scroll-distance='1'>
-		<div class="media" ng-repeat="comment in reddit.comments">
+		<div ng-repeat="comment in reddit.comments">
 			<a class="pull-left" href="#"><img class="media-object" src="http://placehold.it/64x64"></a>
 			<div class="media-body">
 				<h4 class="media-heading"><a href="#"><% comment.author %></a> <small><i class="fa fa-arrow-up"></i> <% comment.score %></small></h4>
