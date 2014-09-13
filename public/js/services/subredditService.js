006d50ec9a148f96cb67b5469f44347fa8d19514
Epick362 <Epick@Filip-MacBook-Pro.local>
2014-09-13 13:21:04 +0200

    ngComments fix

--- public/js/services/subredditService.js
+++ public/js/services/subredditService.js
@@ -48,7 +48,7 @@ angular.module('subredditService', [])
 
 			this.thing = thing;
 
-			var url = '/api/comments/'.this.subreddit.'/'+this.thing;
+			var url = '/api/comments/'+this.subreddit+'/'+this.thing;
 
 			$http({method: 'GET', url: url})
 			.success(function(data) {
