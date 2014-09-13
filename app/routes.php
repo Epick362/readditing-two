5d40900197907523d023c5ebe57f83e236333d24
Epick362 <Epick@Filip-MacBook-Pro.local>
2014-09-13 13:03:53 +0200

    Routes Fix

--- app/routes.php
+++ app/routes.php
@@ -21,7 +21,7 @@ Route::get('/auth/login', 'AuthController@auth');
 // API ROUTES ==================================
 // =============================================
 Route::group(array('prefix' => 'api'), function() {
-	Route::get('/r/{subreddit?}/comments/{thing?}', 'ApiController@indexComment');
+	Route::get('/r/{subreddit}/comments/{thing}', 'ApiController@indexComment');
 	Route::get('/r/{subreddit?}', 'ApiController@subreddit');
 
 	Route::post('/vote/{id}', 'ApiController@storeVote');
