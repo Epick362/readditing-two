392074b02b982d63262729b2c1d5549a88e1c0ad
Epick362 <Epick@Filip-MacBook-Pro.local>
2014-09-13 18:51:07 +0200

    .

--- app/views/partials/comments.blade.php
+++ app/views/partials/comments.blade.php
@@ -11,6 +11,8 @@
 				<h4 class="media-heading"><a href="#"><% comment.author %></a> <small class="text-alternate"><i class="fa fa-arrow-up"></i> <% comment.score %></small></h4>
 				<div ng-bind-html="comment.body"></div>
 
+				<% comment.replies %>
+
 				<li class="media" ng-repeat="reply in comment.replies">
 					<% reply.author %>
 				</li>
