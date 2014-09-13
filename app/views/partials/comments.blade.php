8ffe3b3e77068966f4caaffd34f2f7b34655d457
Epick362 <Epick@Filip-MacBook-Pro.local>
2014-09-13 19:17:45 +0200

    ngComments Fixes

--- app/views/partials/comments.blade.php
+++ app/views/partials/comments.blade.php
@@ -11,9 +11,7 @@
 				<h4 class="media-heading"><a href="#"><% comment.author %></a> <small class="text-alternate"><i class="fa fa-arrow-up"></i> <% comment.score %></small></h4>
 				<div ng-bind-html="comment.body"></div>
 
-				<div ng-repeat="reply in comment.replies">
-					<% reply.author %>
-				</div>
+				<div ng-repeat="reply in comment.replies" ng-include="'comment_tree.html'"></div>
 			</div>
 		</li>
 	</ul>
