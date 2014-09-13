79a7cfb84648f2ec10c9c673157451fc0163e648
Epick362 <Epick@Filip-MacBook-Pro.local>
2014-09-13 14:08:22 +0200

    ngComments Fixes v386

--- app/views/partials/comments.blade.php
+++ app/views/partials/comments.blade.php
@@ -4,17 +4,15 @@
     <h3 class="modal-title"><% post.title %></h3>
 </div>
 <div class="modal-body">
-	<ul class="media-list">
-	    <div infinite-scroll='reddit.getComments(post.id)' infinite-scroll-disabled='reddit.busy' infinite-scroll-distance='1'>
-			<li class="media" ng-repeat="comment in reddit.comments">
-				<a class="pull-left" href="#"><img class="media-object" src="http://placehold.it/64x64"></a>
-				<div class="media-body">
-					<h4 class="media-heading"><a href="#"><% comment.author %></a> <small><i class="fa fa-arrow-up"></i> <% comment.score %></small></h4>
-					<div ng-bind-html="comment.body"></div>
-				</div>
-			</li>
+    <div infinite-scroll='reddit.getComments(post.id)' infinite-scroll-disabled='reddit.busy' infinite-scroll-distance='1'>
+		<div class="media" ng-repeat="comment in reddit.comments">
+			<a class="pull-left" href="#"><img class="media-object" src="http://placehold.it/64x64"></a>
+			<div class="media-body">
+				<h4 class="media-heading"><a href="#"><% comment.author %></a> <small><i class="fa fa-arrow-up"></i> <% comment.score %></small></h4>
+				<div ng-bind-html="comment.body"></div>
+			</div>
 		</div>
-	</ul>
+	</div>
 
 	<!-- LOADING =============================================== -->
 	<div class="loading" ng-show="reddit.busy">
