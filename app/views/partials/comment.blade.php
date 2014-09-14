16590a29dc1c071a3ecd724d7afa9bf13e465ba3
Epick362 <Epick@Filip-MacBook-Pro.local>
2014-09-14 23:30:41 +0200

    .

--- app/views/partials/comment.blade.php
+++ app/views/partials/comment.blade.php
@@ -5,12 +5,12 @@
 	<div ng-bind-html="comment.body"></div>
 	@if(Session::has('user'))
 	<div>
-		<a class="btn btn-link btn-xs" href="#"><i class="fa fa-reply"></i> Reply</a>
+		<a class="btn btn-link btn-xs" href="#">Reply</a>
 		<span ng-if="!comment.saved">
-			<a href="" class="btn btn-link btn-xs" ng-click="save(comment, 't1', 1)"><i class="fa fa-save"></i> Save</a>
+			<a href="" class="btn btn-link btn-xs" ng-click="save(comment, 't1', 1)">Save</a>
 		</span>
 		<span ng-if="comment.saved">
-			<a href="" class="btn btn-link btn-xs active" ng-click="save(comment, 't1', 0)"><i class="fa fa-save"></i> Unsave</a>
+			<a href="" class="btn btn-link btn-xs active" ng-click="save(comment, 't1', 0)">Unsave</a>
 		</span>
 		<a class="btn btn-link btn-xs" href="https://www.reddit.com/gold?goldtype=gift&amp;months=1&amp;thing=t1_<% comment.id %>" target="_blank"><i class="fa fa-gift"></i> Give Gold</a>
 	</div>
