553432a2684250dd95a6ec2b6dc6f0aba386cae7
Epick362 <Epick@Filip-MacBook-Pro.local>
2014-09-15 14:59:56 +0200

    .

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -20,7 +20,7 @@
 	    	My name is <b>Filip Hajek</b> and I am a 19 year old student living in Europe. My hobbies are creating websites and playing video games.
 	    	<br />
 	    	<br />
-	    	If you'd like to contact me, do so either by sending me a private message on reddit (<a href="https://www.reddit.com/message/compose/?to=Epick_362">Epick_362</a>) or send me an email to: <a href="mailto:{{ HTML::email('flp.hajek@gmail.com') }}">{{ HTML::email('flp.hajek@gmail.com') }}</a>.
+	    	If you'd like to contact me, do so either by sending me a private message on reddit (<a href="https://www.reddit.com/message/compose/?to=Epick_362">Epick_362</a>) or send me an email to: {{ HTML::mailto('flp.hajek@gmail.com') }}.
 	    </p>
     </div>
 @stop
