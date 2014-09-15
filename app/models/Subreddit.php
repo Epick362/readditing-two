5046744869d3fc144a37a284f3dd0ad0595798d6
Epick362 <Epick@Filip-MacBook-Pro.local>
2014-09-15 11:14:17 +0200

    .

--- app/models/Subreddit.php
+++ app/models/Subreddit.php
@@ -76,6 +76,8 @@ class Subreddit extends Eloquent {
 	public static function isSubscribedToSubreddit($subreddit) {
 		$mine = Reddit::fetch('reddits/mine.json'); 
 
+		dd($mine);
+
 		if($mine) {
 			foreach($mine['data']['children'] as $sub) {
 				if($sub['data']['display_name'] == $subreddit) {
