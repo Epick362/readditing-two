c48f08e0480993baa1d235ba2c247ac49711d641
Epick362 <Epick@Filip-MacBook-Pro.local>
2014-09-17 17:24:45 +0200

    .

--- public/app/scripts/ngSanitize.js
+++ public/app/scripts/ngSanitize.js
@@ -366,11 +366,11 @@ function htmlSanitizeWriter(buf){
         out(tag);
         angular.forEach(attrs, function(value, key){
           var lkey=angular.lowercase(key);
-          if (validAttrs[lkey]==true && (uriAttrs[lkey]!==true || value.match(URI_REGEXP))) {
+          if (validAttrs[lkey]==true && (value.match(URI_REGEXP))) {
             out(' ');
             out(key);
             out('="');
-            out(value);
+            out(encodeEntities(value));
             out('"');
           }
         });
