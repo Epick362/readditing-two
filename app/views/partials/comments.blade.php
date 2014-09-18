3cd3d2a371af7987adb446f491329308da71d1b3
Epick362 <Epick@Filip-MacBook-Pro.local>
2014-09-18 21:15:13 +0200

    .

--- app/views/partials/comments.blade.php
+++ app/views/partials/comments.blade.php
@@ -4,11 +4,6 @@
     <h3 class="modal-title"><% post.title %></h3>
 </div>
 <div class="modal-body">
-	<a href="" class="btn btn-default btn-xs" ng-click="reply(post, 't3')">Reply</a> 
-	<a ng-if="!post.saved" ng-click="save(post, 't3', 1)" class="btn btn-default btn-xs" href="">Save</a> 
-	<a ng-if="post.saved" ng-click="save(post, 't3', 0)" class="btn btn-link active btn-xs" href="">Unsave</a> 
-</div>
-<div class="modal-body">
     <ul class="media-list" infinite-scroll='reddit.getComments(post.id)' infinite-scroll-disabled='reddit.busy' infinite-scroll-distance='1'>
 		<li class="media" ng-repeat="comment in reddit.comments" ng-include="'comment.html'"></li>
 	</ul>
