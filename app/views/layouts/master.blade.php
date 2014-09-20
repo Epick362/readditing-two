e5418e4f4fa91641cc60b6e264aa67575816c893
Epick362 <Epick@Filip-MacBook-Pro.local>
2014-09-20 16:19:44 +0200

    .

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -12,7 +12,6 @@
 		<meta property="og:title" content="Readditing.com | {{ $subreddit or 'better reddit experience' }}" />
 		<meta property="og:type" content="website" />
 		<meta property="og:url" content="http://www.readditing.com/" />
-		<meta property="og:image" content="http://www.readditing.com/apple-touch-icon-120x120.png" />
 		<meta property="og:description" content="Readditing is a social reddit website" />
 
 		<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
@@ -97,5 +96,16 @@
 				</div>
 			</div>
 		</div>
+
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-46225304-3', 'auto');
+		  ga('send', 'pageview');
+
+		</script>
 	</body>
 </html>
