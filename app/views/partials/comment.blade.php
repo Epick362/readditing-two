9c77f69dab5f12180463a306cbf6d954ba1c8500
Epick362 <Epick@Filip-MacBook-Pro.local>
2014-09-21 10:59:16 +0200

    Comment Replies Fix

--- app/views/partials/comment.blade.php
+++ app/views/partials/comment.blade.php
@@ -20,5 +20,5 @@
 	</div>
 	@endif
 	<div class="replyForm"></div>
-	<div ng-if="comment.replies && comment.replies.length > 0" class="media" ng-repeat="comment in comment.replies" ng-include="'comment.html'"></div>
+	<div ng-if="comment.replies" class="media" ng-repeat="comment in comment.replies" ng-include="'comment.html'"></div>
 </div>
