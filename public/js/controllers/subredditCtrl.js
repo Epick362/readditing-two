89414bad635e9dc2435c3146b63087def7e9fe3f
Epick362 <flp.hajek@gmail.com>
2014-09-23 19:09:40 +0200

    .

--- public/js/controllers/subredditCtrl.js
+++ public/js/controllers/subredditCtrl.js
@@ -160,7 +160,7 @@ angular.module('subredditCtrl', [])
 	            element.bind('click', function(e) {
 	                e.stopPropagation();
 
-	                if(attrs['reply-form'] === 'post') {
+	                if(attrs['replyForm'] === 'post') {
 	                	element.closest('.modal-dialog').find('.media-list').prepend($compile('<li ng-show="!comment.replied" class="media"><div class="media-body"><form ng-submit="reply(post, \'t3\')"><div class="form-group" style="margin:10px 0 0 30px"><textarea class="form-control" ng-model="comment.reply" rows="3"></textarea><button style="margin-top:10px" class="btn btn-primary">Send</button></div></form></div></li>')(scope));
 	                }else{
 	                	element.closest('.media-body').find('.replyForm:first').html($compile('<form ng-show="!comment.replied" ng-submit="reply(comment, \'t1\')"><div class="form-group" style="margin:10px 0 0 30px"><textarea class="form-control" ng-model="comment.reply" rows="3"></textarea><button style="margin-top:10px" class="btn btn-primary">Send</button></div></form>')(scope));
