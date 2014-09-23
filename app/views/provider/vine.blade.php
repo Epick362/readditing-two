366e7aa1e710648c958ea0c094d37d248769bb20
Epick362 <flp.hajek@gmail.com>
2014-09-23 18:26:20 +0200

    .

--- app/views/provider/vine.blade.php
+++ app/views/provider/vine.blade.php
@@ -1,2 +1,4 @@
-<iframe class="vine-embed" src="https://vine.co/v/{{ $data['vine-id'] }}/embed/simple" width="600" height="600" frameborder="0"></iframe>
-<script async src="//platform.vine.co/static/scripts/embed.js" charset="utf-8"></script>
+<div class="block-centered">
+	<iframe class="vine-embed" src="https://vine.co/v/{{ $data['vine-id'] }}/embed/simple" width="600" height="600" frameborder="0"></iframe>
+	<script async src="//platform.vine.co/static/scripts/embed.js" charset="utf-8"></script>
+</div>
