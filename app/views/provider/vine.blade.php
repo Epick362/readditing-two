17c4c657a90299fe33d73935acec86df7ea13882
Epick362 <flp.hajek@gmail.com>
2014-09-23 18:27:14 +0200

    .

--- app/views/provider/vine.blade.php
+++ app/views/provider/vine.blade.php
@@ -1,4 +1,4 @@
-<div class="block-centered">
+<div class="center-block">
 	<iframe class="vine-embed" src="https://vine.co/v/{{ $data['vine-id'] }}/embed/simple" width="600" height="600" frameborder="0"></iframe>
 	<script async src="//platform.vine.co/static/scripts/embed.js" charset="utf-8"></script>
 </div>
