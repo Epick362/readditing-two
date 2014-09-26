221b41261cc74a22308b31cb1ed676220d83a637
Epick362 <flp.hajek@gmail.com>
2014-09-26 20:29:12 +0200

    HTML Fixes

--- app/views/profile.blade.php
+++ app/views/profile.blade.php
@@ -10,13 +10,13 @@
 @overwrite
 
 @section('content')
-	<div class="col-md-8 col-md-offset-2">
+	<div class="col-md-8 col-md-offset-2" ng-cloak>
 		<ul class="nav nav-tabs nav-justified" style="margin-bottom:10px">
 			<li><strong>{{ $user }}</strong></li>
-			<li class="active"><a href="{{ URL::to('u/'.$user) }}">Overview</a></li>
-			<li class=""><a href="{{ URL::to('u/'.$user.'/comments') }}">Comments</a></li>
-			<li class=""><a href="{{ URL::to('u/'.$user.'/submitted') }}">Submitted</a></li>
-			<li class=""><a href="{{ URL::to('u/'.$user.'/gilded') }}">Gilded</a></li>
+			<li class="{{ Request::is($user.'/') ? 'active' : '' }}"><a href="{{ URL::to('u/'.$user) }}">Overview</a></li>
+			<li class="{{ Request::is($user.'/saved') ? 'active' : '' }}"><a href="{{ URL::to('u/'.$user.'/saved') }}">Saved</a></li>
+			<li class="{{ Request::is($user.'/submitted') ? 'active' : '' }}"><a href="{{ URL::to('u/'.$user.'/submitted') }}">Submitted</a></li>
+			<li class="{{ Request::is($user.'/gilded') ? 'active' : '' }}"><a href="{{ URL::to('u/'.$user.'/gilded') }}">Gilded</a></li>
 		</ul>
 
 		<script type="text/ng-template" id="comment.html">
