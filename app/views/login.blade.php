13016044bb13a19c5b660751c01ea02b2d4f8c38
Epick362 <flp.hajek@gmail.com>
2014-09-28 11:25:07 +0200

    Login Page update

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -5,15 +5,15 @@
 		<h1 style="margin-top:0">Login</h1>
 		<hr />
 		<p class="lead">
-			Logging in will enable you to upvote post, submit new ones and save content you like for later!
+			Logging in will enable you to upvote posts, comment, submit new ones and save content you like for later!
 	    </p>
+
+		<div class="text-center">
+			<a class="btn btn-default btn-lg" href="{{ URL::to('auth/login') }}">Login with Reddit</a>
+		</div>
 
 		<p>
 			Readditing enables you to <b>securely</b> log in to your reddit account without you having to provide your login credentials on our site. Click the button below to begin.
 	    </p>
-
-		<div class="text-center">
-			<a class="btn btn-default btn-lg" href="{{ URL::to('auth/login') }}">Login with Reddit account</a>
-		</div>
     </div>
 @stop
