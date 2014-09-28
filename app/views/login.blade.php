a082fada90373ab62a76c09e8fa9cbfffa0e5426
Epick362 <flp.hajek@gmail.com>
2014-09-28 11:24:08 +0200

    Login Page

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -5,9 +5,15 @@
 		<h1 style="margin-top:0">Login</h1>
 		<hr />
 		<p class="lead">
-			Readditing enables you to securely log in to your reddit account without you having to provide your login credentials on our site. Click the button below to begin.
-
-			<a class="btn btn-default btn-block btn-lg" href="{{ URL::to('auth/login') }}">Login with Reddit account</a>
+			Logging in will enable you to upvote post, submit new ones and save content you like for later!
 	    </p>
+
+		<p>
+			Readditing enables you to <b>securely</b> log in to your reddit account without you having to provide your login credentials on our site. Click the button below to begin.
+	    </p>
+
+		<div class="text-center">
+			<a class="btn btn-default btn-lg" href="{{ URL::to('auth/login') }}">Login with Reddit account</a>
+		</div>
     </div>
 @stop
