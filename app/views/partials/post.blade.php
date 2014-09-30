09f2f3d19fc6b1c136f76cefb3b4591de2985f58
Epick362 <flp.hajek@gmail.com>
2014-09-30 22:22:10 +0200

    Responsive Upvotes

--- app/views/partials/post.blade.php
+++ app/views/partials/post.blade.php
@@ -1,6 +1,6 @@
 	<div infinite-scroll="{{ $function or 'reddit.nextPage()' }}" infinite-scroll-disabled='reddit.busy' infinite-scroll-distance='1'>
 		<div class="panel panel-default" ng-class="(post.nsfw) ? 'nsfw' : ''" ng-repeat="post in reddit.posts">
-			<div class="upvote-wrapper" ng-if="post.author">
+			<div class="upvote-wrapper hidden-sm hidden-xs" ng-if="post.author">
 				<div class="upvote-container">
 					<span ng-if="!post.likes">
 						<a href="" class="btn btn-upvote lg" ng-click="vote(post, 't3', 1)" analytics-on analytics-event="Upvote"><i class="fa fa-arrow-up"></i></a>
@@ -19,15 +19,29 @@
 			<div class="panel-body" show-more ng-bind-html="post.content">
 
 			</div>
+			<div class="panel-body hidden-lg hidden-md" ng-if="post.author">
+				<div class="row">
+					<div class="col-xs-12 text-center">
+						<span ng-if="!post.likes">
+							<a href="" class="btn btn-upvote btn-xs" ng-click="vote(post, 't3', 1)" analytics-on analytics-event="Upvote"><i class="fa fa-arrow-up"></i></a>
+							<small class="text-alternate"><% post.score %> upvotes</small>
+						</span>
+						<span ng-if="post.likes">
+							<a href="" class="btn btn-upvote btn-xs active" ng-click="vote(post, 't3', 0)" analytics-on analytics-event="UnUpvote"><i class="fa fa-arrow-up"></i></a>
+							<small class="text-alternate"><% post.score %> upvotes</small>
+						</span>						
+					</div>
+				</div>
+			</div>
 			<div class="panel-footer" ng-if="post.author">
 				<div class="row">
-					<div class="col-sm-4">
+					<div class="col-xs-4">
 						<a ng-href="{{ URL::to('u') }}/<% post.author %>"><% post.author %></a> in <a href="{{ URL::to('r') }}/<% post.subreddit %>"><% post.subreddit %></a> 
 					</div>
-					<div class="col-sm-4 text-center">
+					<div class="col-xs-4 text-center">
 						<a href="" ng-click="comments(post)"><i class="fa fa-comment-o"></i> <% post.comments %> comments</a>
 					</div>
-					<div class="col-sm-4 text-right">
+					<div class="col-xs-4 text-right">
 						@if(Session::has('user'))
 							<span ng-if="!post.saved">
 								<a ng-click="save(post, 't3', 1)" href="" analytics-on analytics-event="Save"><i class="fa fa-save"></i> Save</a>
