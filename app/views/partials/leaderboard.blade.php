4abd28e734d9b43cf9e8ab415d8c03121b1521d4
Epick362 <flp.hajek@gmail.com>
2014-10-22 21:29:55 +0200

    Google Adsense

--- app/views/partials/leaderboard.blade.php
+++ app/views/partials/leaderboard.blade.php
@@ -1,10 +1,10 @@
 <div class="ad-leaderboard">
 	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-	<!-- Leaderboard Main -->
+	<!-- getAmused.net -->
 	<ins class="adsbygoogle"
 	     style="display:inline-block;width:728px;height:90px"
 	     data-ad-client="ca-pub-6468416760530377"
-	     data-ad-slot="4560424843"></ins>
+	     data-ad-slot="3409274445"></ins>
 	<script>
 	(adsbygoogle = window.adsbygoogle || []).push({});
 	</script>
