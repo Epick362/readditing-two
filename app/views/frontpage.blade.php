8789770d2b5ea533e6c0409ad2fcb926d8ce89c0
Epick362 <flp.hajek@gmail.com>
2014-10-22 20:49:14 +0200

    GET after fix

--- app/views/frontpage.blade.php
+++ app/views/frontpage.blade.php
@@ -77,7 +77,7 @@
 				@include('partials.comments')
 			</script>
 
-			@if(Input::has('after'))
+			@if(Input::has('after') && Input::get('after') !== 'undefined')
 				<a style="margin-bottom: 20px" class="btn btn-primary btn-block" href="{{ URL::to('r/'.$channel) }}">Jump back to top</a>
 			@endif
 			
