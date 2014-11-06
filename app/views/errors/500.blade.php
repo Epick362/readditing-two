3ebdae90ec0e8c886f6ff26498a35fd6e95c775e
Epick362 <flp.hajek@gmail.com>
2014-11-06 22:19:23 +0100

    .

--- app/views/errors/500.blade.php
+++ app/views/errors/500.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
 	<div class="col-md-8 col-md-offset-2">
-		<p class="lead text-center" style="font-size:42px">There was a <b>server error</b> while processing your request. This is not your fault and it would be a good idea to contact the me at {{ HTML::mailto('admin@readditing.com') }}.</p>
+		<p class="lead text-center" style="font-size:42px">Readditing.com is down because we have reached our API limits. We are working to resolve this ASAP.</p>
+		<p class="lead text-center" style="font-size:22px">There was a <b>server error</b> while processing your request. This is not your fault and it would be a good idea to contact the me at {{ HTML::mailto('admin@readditing.com') }}.</p>
 		<br />
 		<div class="embed-responsive embed-responsive-16by9 hidden-xs">
 			<iframe class="embed-responsive-item" width="560" height="315" src="//www.youtube-nocookie.com/embed/aBK9O5nDJlM" frameborder="0"></iframe>
