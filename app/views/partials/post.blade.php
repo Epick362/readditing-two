5dc9259c68f3172617a072226eede84990c0dd51
Epick362 <flp.hajek@gmail.com>
2015-01-17 00:02:07 +0100

    Prettier Vote buttons

--- app/views/partials/post.blade.php
+++ app/views/partials/post.blade.php
@@ -1,28 +1,26 @@
 	<div infinite-scroll="{{ $function or 'reddit.nextPage()' }}" infinite-scroll-disabled='reddit.busy' infinite-scroll-distance='1'>
 		<div class="panel panel-default" ng-class="(post.nsfw) ? 'nsfw' : ''" ng-repeat="post in reddit.posts">
 			<div class="upvote-wrapper hidden-sm hidden-xs" ng-if="post.author">
-				<div class="upvote-container">
+				<div class="upvote-container text-center">
 					<!-- Upvote -->
-					<span ng-if="post.likes == null || post.likes == false">
+					<div ng-if="post.likes == null || post.likes == false">
 						<a href="" class="btn btn-upvote lg" ng-click="vote(post, 't3', 1)" analytics-on analytics-category="Upvote" analytics-label="Up"><i class="fa fa-arrow-up"></i></a>
-					</span>
-					<span ng-if="post.likes == true">
+					</div>
+					<div ng-if="post.likes == true">
 						<a href="" class="btn btn-upvote lg active" ng-click="vote(post, 't3', 0)" analytics-on analytics-category="Upvote" analytics-label="Un"><i class="fa fa-arrow-up"></i></a>
-					</span>
-
-					<br />
+					</div>
 
 					<!-- Downvote -->
-					<span ng-if="post.likes == null || post.likes == true">
+					<div ng-if="post.likes == null || post.likes == true">
 						<a href="" class="btn lg btn-downvote" ng-click="vote(post, 't3', -1)" analytics-on analytics-category="Downvote" analytics-label="Down"><i class="fa fa-arrow-down"></i></a>
-					</span>
-					<span ng-if="post.likes == false">
+					</div>
+					<div ng-if="post.likes == false">
 						<a href="" class="btn lg btn-downvote active" ng-click="vote(post, 't3', 0)" analytics-on analytics-category="Downvote" analytics-label="Un"><i class="fa fa-arrow-down"></i></a>
-					</span>
+					</div>
 
-					<p class="text-center" style="margin-top:5px"><% post.score %></p>
+					<p style="margin-top:5px"><% post.score %></p>
 
-					<div class="text-center">
+					<div>
 					@if(Session::has('user'))
 						<span ng-if="!post.saved">
 							<a ng-click="save(post, 't3', 1)" class="btn btn-default btn-xs" href="" analytics-on analytics-category="Save">Save</a>
