c245e0643e353723243af58c564f720df79b315b
Epick362 <flp.hajek@gmail.com>
2015-02-07 20:02:01 +0100

    503

---
+++ app/views/errors/503.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('content')
+	<div class="col-md-8 col-md-offset-2">
+		It looks like reddit started blocking our API access. There is not much we can do about this. I will try to take things work again ASAP.
+
+		Subscribe to <a href="https://reddit.com/r/readditingcom">readditing.com subreddit</a>.
+    </div>
+@stop
