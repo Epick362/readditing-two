0ebe174383f9d881cc7bd14efdbf9ff94762e4d7
Epick362 <flp.hajek@gmail.com>
2015-02-07 20:05:40 +0100

    .

--- app/start/global.php
+++ app/start/global.php
@@ -76,7 +76,7 @@ App::error(function(Exception $exception, $code)
 
 App::down(function()
 {
-	return Response::make("Be right back!", 503);
+	return Response::view('errors.503', [], 503);
 });
 
 /*
