c4e6ff15338fa0789b7c31b6f82cfed86b076bc1
Epick362 <flp.hajek@gmail.com>
2015-02-09 19:12:31 +0100

    .

--- app/views/errors/503.blade.php
+++ app/views/errors/503.blade.php
@@ -2,8 +2,10 @@
 
 @section('content')
 	<div class="col-md-8 col-md-offset-2">
-		It looks like reddit started blocking our API access. There is not much we can do about this. I will try to take things work again ASAP.
+		<p class="lead">
+			It looks like reddit started blocking our API access. There is not much we can do about this. I will try to take things work again ASAP.
 
-		Subscribe to <a href="https://reddit.com/r/readditingcom">readditing.com subreddit</a>.
+			Subscribe to <a href="https://reddit.com/r/readditingcom">readditing.com subreddit</a>.
+		</p>
     </div>
 @stop
