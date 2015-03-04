66ee39a9a5edfe66907ada527e7f0e36b5a106e0
Epick362 <flp.hajek@gmail.com>
2015-03-04 10:56:12 +0100

    503 Update

--- app/views/errors/503.blade.php
+++ app/views/errors/503.blade.php
@@ -1,11 +1,21 @@
 @extends('layouts.master')
 
+@section('title')
+	Site temporarily unavailable
+@stop
+
 @section('content')
 	<div class="col-md-8 col-md-offset-2">
+		<h1 style="margin-top:0">Site temporarily unavailable</h1>
+		<hr />
 		<p class="lead">
-			It looks like reddit started blocking our API access. There is not much we can do about this. I wrote about the future of Readditing <a href="http://filiphajek.com/readditing-adventure/">here</a><br />
+			It looks like reddit started blocking our API access. There is not much we can do about this.<br /><br />
 
-			<a href="http://filiphajek.com">Follow my personal blog for future updates.</a>
+			<strong><small>Update (4th March 2015): The solution is still being worked on but I am on a vacation now.</small></strong>
 		</p>
+
+		<div class="text-center">
+			<h2><a href="http://filiphajek.com">Follow my personal blog for future updates.</a></h2>
+		</div>
     </div>
 @stop
