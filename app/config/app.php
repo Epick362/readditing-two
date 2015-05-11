e23351d0feecf8fc2cf9be3ec84f04d1695b0522
Epick362 <flp.hajek@gmail.com>
2015-05-11 11:49:12 +0200

    Dependency Fix

--- app/config/app.php
+++ app/config/app.php
@@ -106,8 +106,6 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
-
-		'Way\Generators\GeneratorsServiceProvider',
 		
 		'Readditing\Readability\ReadabilityServiceProvider',
 		'Readditing\Reddit\RedditServiceProvider',
