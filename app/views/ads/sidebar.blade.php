5371d129978e4be18a6f545731a34e855b341d0d
Epick362 <flp.hajek@gmail.com>
2015-05-21 11:58:47 +0200

    No Ad in Sidebar + CSS Ad

--- app/views/ads/sidebar.blade.php
+++ app/views/ads/sidebar.blade.php
@@ -1,12 +1,3 @@
 <div class="ad-sidebar">
-	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-	<!-- Redditing -->
-	<ins class="adsbygoogle"
-	     style="display:block"
-	     data-ad-client="ca-pub-6468416760530377"
-	     data-ad-slot="3654542445"
-	     data-ad-format="auto"></ins>
-	<script>
-	(adsbygoogle = window.adsbygoogle || []).push({});
-	</script>
+	
 </div>
