ba2c8a5de76567465bd5adac9f7a9114932776d1
Epick362 <flp.hajek@gmail.com>
2015-05-21 16:24:40 +0200

    Little improvements of About page

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -9,19 +9,16 @@
 		<h1 style="margin-top:0">About</h1>
 		<hr />
 		<p class="lead">
-			<strong>Redditing&trade;</strong> is a <a href="https://reddit.com" target="_blank" rel="nofollow"><strong>reddit</strong></a> browser made to simplify browsing reddit using various API's and visual improvements.
+			<strong>Redditing</strong> is a <a href="https://reddit.com" target="_blank" rel="nofollow"><strong>reddit</strong></a> browser made to simplify browsing reddit using various API's and visual improvements.
 		    <br />
 		    <br />
 		    Redditing began as a weekend project of mine and has since changed into website with, at the time of writing this, <strong>10 000+</strong> unique of visitors every day. I am very happy that people like what I've created and I try to improve Redditing every day.
-		    <br />
-		    <br />
-		    You can read more about the project on <a href="{{ URL::to('r/readditingcom') }}"><strong>Redditing.com</strong></a> subreddit.
 	    </p>
 
 	    <h3>About the Author</h3>
 	    <hr />
 	    <p class="lead">
-	    	My name is <a href="http://filiphajek.me/"><strong>Filip Hajek</strong></a> and I am a 19 year old student living in Europe. My hobbies are creating websites and playing video games.
+	    	My name is <a href="http://filiphajek.com/"><strong>Filip Hajek</strong></a> and I am a 19 year old student living in Europe. My hobbies are creating websites and playing video games.
 	    	<br />
 	    	<br />
 	    	If you'd like to contact me, do so either by sending me a private message on reddit (<a href="https://www.reddit.com/message/compose/?to=Epick_362">Epick_362</a>) or send me an email to: {{ HTML::mailto('flp.hajek@gmail.com') }}.
