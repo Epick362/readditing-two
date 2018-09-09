0ad91284456835219071057ed7b7e1dcf9d99e72
Epick362 <flp.hajek@gmail.com>
2018-09-09 13:41:30 +0200

    Fix imgur gifv

---
+++ app/views/provider/imgur-video.blade.php
@@ -0,0 +1,3 @@
+<video style="display:block;margin:0 auto;" poster="//i.imgur.com/{{ $data['videoid'] }}.jpg" preload="auto" autoplay="autoplay" muted="muted" loop="loop" webkit-playsinline="" controls="">
+  <source src="//i.imgur.com/{{ $data['videoid'] }}.mp4" type="video/mp4">
+</video>
