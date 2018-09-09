05ddb71bf60bd448fef843ea6b9636f4920db3f9
Epick362 <flp.hajek@gmail.com>
2018-09-09 13:46:59 +0200

    Fix imgur gifv

--- workbench/readditing/formatter/src/Readditing/Formatter/Provider/imgur.php
+++ workbench/readditing/formatter/src/Readditing/Formatter/Provider/imgur.php
@@ -62,7 +62,7 @@ class Imgur extends Provider {
 
     return array(
 			'title' => $this->data['data']['title'],
-			'content' => \View::make('provider.imgurVideo', ['data' => $videoId])->render(),
+			'content' => \View::make('provider.imgurVideo', ['data' => ['video-id' => $videoId]])->render(),
 			'source' => 'imgur.com'
 		);
   }
